cad10a3a9bb1c2b56d375fc779da5e1f2e36d8af
Geovane Borba <geovanne@live.com>
2023-11-24 10:05:13 -0300

    chore: add utils

---
+++ src/utils/handleSubscriptions.ts
@@ -0,0 +1,45 @@
+import { db } from "@/services/mongodb";
+import stripe from "@/services/stripe";
+
+export async function saveSubscription(
+  subscriptionId: string,
+  customerId: string
+) {
+  const user = await db.collection("users").findOne({
+    stripe_customer_id: customerId,
+  });
+
+  const subscription = await stripe.subscriptions.retrieve(
+    subscriptionId as string
+  );
+
+  const subscriptionData = {
+    id: subscription.id,
+    userId: user?._id,
+    status: subscription.status as string,
+    price_id: subscription.items.data[0].price.id,
+  };
+
+  await db.collection("subscription").insertOne(subscriptionData);
+}
+
+export async function updateSubscription(subscriptionId: string) {
+  const subscription = await stripe.subscriptions.retrieve(
+    subscriptionId as string
+  );
+
+  const subscriptionData = {
+    id: subscription.id,
+    status: subscription.status as string,
+    price_id: subscription.items.data[0].price.id,
+  };
+
+  await db.collection("subscription").updateOne(
+    {
+      id: subscriptionId,
+    },
+    {
+      $set: subscriptionData,
+    }
+  );
+}
