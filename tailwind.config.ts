5e1444c707c4d996a7300edc730f69d5164f5c48
Geovane Borba <geovanne@live.com>
2023-11-24 09:57:40 -0300

    chore: add tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,40 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ["var(--font-roboto)"],
+      },
+      colors: {
+        white: "#FFFFFF",
+        green: {
+          500: "#04d361",
+        },
+        gray: {
+          100: "#e1e1e6",
+          200: "#737380",
+          300: "#a8a8b3",
+          700: "#323238",
+          800: "#29292e",
+          850: "#1f2729",
+          900: "#121214",
+        },
+        cyan: {
+          500: "#61dafb",
+        },
+        yellow: {
+          500: "#eba417",
+        },
       },
     },
   },
+
   plugins: [],
-}
-export default config
+};
+export default config;
